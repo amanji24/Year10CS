c46c7562c8fb2b612e9eab6d3cf36624e3b5f35b
Adam Manji <adam.manji@09805-161.local>
2018-10-04 13:57:12 -0400

    Just adding a Helloworld

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class Helloworld {
+	 public static void main(String [] args) {
+		 for(int i = 1; i < 11; i++) {
+		 System.out.println("What's up guys, Ali A here and todayyy we are looking at LEAKED SKINS in *FORTNITE BATTLE ROYALE* " + i);
+		 }
+	 }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Year10CS
 Year 10 CompSci
+What's up guys ALI A HEEEREE and TODAYYYY we're looking at leaked season 7 battle pass in *FORTNITE BATTLE ROYALE*
